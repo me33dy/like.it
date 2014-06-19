bb3108fd03fc5494449f4b7ef068f66ebddd387c
Eric Wu <ericwu@Erics-MacBook-Pro.local>
2014-06-18 18:28:29 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Likeit::Application.config.secret_key_base = 'b2fddc543640f13fa2feaeb5286722d43cf225ee2f2c8937be69a42ba5f2cee299866253da4cbef80f19360d1633ec3863d1a25ea3e18e8aa3e0d094c6c2d96e'
