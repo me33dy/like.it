d6862c5f9438f8b36aee135a1c62c395ce482e5a
ttran289 <thachtran289@gmail.com>
2014-06-24 16:00:33 -0700

    user index product index

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,7 +2,7 @@ class UserSessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to root_url
+    redirect_to user_index_url
   end
 
   def destroy
