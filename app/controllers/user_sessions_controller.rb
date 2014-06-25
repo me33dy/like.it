2b2631eabef386adfbbc7725686c8546a2f32395
Aaron Tsang <aarontsang@Aarons-MacBook-Pro.local>
2014-06-25 16:51:04 -0700

    more layout fixes

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,7 +2,7 @@ class UserSessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to user_index_url
+    redirect_to users_path
   end
 
   def destroy
