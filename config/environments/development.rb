79b6097bfebfbb2cd3de690148db0ea6159ff69d
Aaron <aarontsang8@gmail.com>
2014-07-21 20:04:34 -0700

    trying to fix bucket issues

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Likeit::application.configure do
+Likeit::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
---
+++ config/initializers/aws.rb
@@ -0,0 +1,3 @@
+AWS.config(
+access_key_id: 'AKIAJCLR6ECKHL2JLN6A',
+secret_access_key: 'iKGep1NQBvhgWfFYlHPjU0n+/bJP+pWvLjhO16n2')
